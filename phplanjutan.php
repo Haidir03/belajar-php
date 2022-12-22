293fdd1f681c58e61ae11d8fe647cda97fab28f3
Laode Muhammad Haidir <ld.muhammadhaidir03@gmail.com>
2022-12-22 09:21:03 +0800

    membuat form handling

--- phplanjutan.php
+++ phplanjutan.php
@@ -40,6 +40,7 @@ if ($nama == haidir ){
     echo $nama. " darimana yah ?";
 }
 */
+/*
 $nama = "Haidir"
 switch ($nama) {
     case "Haidir";
@@ -52,5 +53,42 @@ switch ($nama) {
     $pesan  = $nama." darimana yah?"; 
 }
 echo $pesan;
-
+*/
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action = "<?php $SERVER ['PHP_SELF'] ?>" method="post">
+    <label>nama</label>
+    <input type="text" name="nama">
+    <label>Jumlah</label>
+    <input type ="text" name ="no">
+    <input type="submit" name="submit" value="Submit">
+</form>
+<?php
+    if(!empty ($_POST['submit'] == "Submit")) {
+        switch ($POST['nama']) {
+            case "Haidir";
+                $pesan = $nama." adalah orang kendari";
+            break;
+            case  "putu";
+                $pesan = $nama." adalah orang bali";
+            break;
+            default 
+            $pesan  = $nama." darimana yah?"; 
+        }
+        for ($i=0;$i<$_POST['no'];$i++){
+            echo $pesan."<br>";
+        }
+    } else {
+        echo "Anda belum input nama dan jumlah";
+    }
+
+</body>
+</html>
